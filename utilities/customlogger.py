0752cbe1f459992fa50ae1e9ff8665a9322db9cb
rock0232 <tony.5677774@gmail.com>
2023-02-08 11:05:54 +0530

    change run.bat

--- utilities/customlogger.py
+++ utilities/customlogger.py
@@ -3,7 +3,7 @@ import logging
 class Logger:
     @staticmethod
     def logen():
-        logging.basicConfig(filename=".//Logs//automation.log",
+        logging.basicConfig(filename="..//Logs//automation.log",
                             format='%(asctime)s: %(levelname)s: %(message)s', datefmt='%m/%d/%Y %I:%M:%S %p', force=True, level=logging.INFO)
         logger = logging.getLogger()
         return logger
