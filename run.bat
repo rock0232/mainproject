1ac6c8b0dbbcbba16e119c329a28701c0c5fccdf
rock0232 <113226804+rock0232@users.noreply.github.com>
2023-02-08 11:00:06 +0530

    update run.bat file

--- run.bat
+++ run.bat
@@ -1,1 +1,1 @@
-python -m pytest -m b2bmarker -s -v -n=5 --capture=sys --html=Reports\B2Btestreport.html --self-contained-html testcases\B2Btest.py
+python -m pytest -m b2bmarker -s -v -n=2 --capture=sys --html=Reports\B2Btestreport.html --self-contained-html testcases\B2Btest.py
