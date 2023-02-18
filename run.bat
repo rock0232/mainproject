da53182310e8c1b3d8ac4707c36bc98cc44b09e8
rock0232 <tony.5677774@gmail.com>
2023-02-18 13:45:37 +0530

    Remove Unnacessory Files From Project

--- run.bat
+++ run.bat
@@ -1,2 +1,15 @@
 @REM pytest -m user365 -s -v --capture=sys --html=Reports\B2BTestReport.html --self-contained-html testcases\B2Btest.py
-pytest -m aura25 -s -v --capture=sys --html=Reports\B2BTestReport.html --self-contained-html TestCases\
+pytest -m abdexch -s -v --capture=sys --html=Reports\abdexchTestReport.html --self-contained-html TestCases\
+pytest -m aura25 -s -v --capture=sys --html=Reports\aura25TestReport.html --self-contained-html TestCases\
+pytest -m badabet -s -v --capture=sys --html=Reports\badabetTestReport.html --self-contained-html TestCases\
+pytest -m infinity -s -v --capture=sys --html=Reports\infinityTestReport.html --self-contained-html TestCases\
+pytest -m user365 -s -v --capture=sys --html=Reports\user365TestReport.html --self-contained-html TestCases\
+pytest -m only333 -s -v --capture=sys --html=Reports\only333TestReport.html --self-contained-html TestCases\
+pytest -m pk7 -s -v --capture=sys --html=Reports\pk7TestReport.html --self-contained-html TestCases\
+pytest -m gamex -s -v --capture=sys --html=Reports\gamexTestReport.html --self-contained-html TestCases\
+pytest -m aura26 -s -v --capture=sys --html=Reports\aura26TestReport.html --self-contained-html TestCases\
+pytest -m xtra999 -s -v --capture=sys --html=Reports\xtra999TestReport.html --self-contained-html TestCases\
+pytest -m run567 -s -v --capture=sys --html=Reports\run567TestReport.html --self-contained-html TestCases\
+pytest -m thalaiva -s -v --capture=sys --html=Reports\thalaivaTestReport.html --self-contained-html TestCases\
+pytest -m empire -s -v --capture=sys --html=Reports\empireTestReport.html --self-contained-html TestCases\
+pytest -m hattrick -s -v --capture=sys --html=Reports\hattrickTestReport.html --self-contained-html TestCases\
