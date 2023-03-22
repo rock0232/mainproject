5f62c3fccfe9ba369591ee97b9768a6487c5451b
rock0232 <tony.5677774@gmail.com>
2023-03-22 17:23:34 +0530

    Changes in code

--- TestCases/conftest.py
+++ TestCases/conftest.py
@@ -10,14 +10,28 @@ from pytest_html import html_report
 def get_project_root() -> Path:
     return Path(__file__).parent.parent
 
+# @pytest.fixture()
+# def setup():
+#     global driver
+#     filepath = get_project_root()
+#     serv_obj = Service(f'{filepath}/chromedriver.exe')
+#     driver = webdriver.Chrome(service=serv_obj)
+#     driver.implicitly_wait(10)
+#     return driver
+
 @pytest.fixture()
 def setup():
-    global driver
-    filepath = get_project_root()
-    serv_obj = Service(f'{filepath}/chromedriver.exe')
-    driver = webdriver.Chrome(service=serv_obj)
-    driver.implicitly_wait(10)
-    return driver
+    # Initialize Chrome Driver
+    chrome_options = webdriver.ChromeOptions()
+    chrome_options.add_argument('--headless')
+    driver = webdriver.Chrome(options=chrome_options)
+
+    # Return Chrome Driver instance
+    yield driver
+
+    # Teardown Chrome Driver instance
+    # driver.quit()
+
 
 def pytest_html_report_title(report):
     report.title = "Test Result Report"
