bb1ea9046c5efe3da8f611f8fe7e5db34a7ae93e
rock0232 <tony.5677774@gmail.com>
2023-03-23 13:04:32 +0530

    Changes in code

--- TestCases/conftest.py
+++ TestCases/conftest.py
@@ -15,7 +15,7 @@ def setup():
     global driver
     filepath = get_project_root()
     # serv_obj = Service(f'{filepath}/chromedriver.exe')
-    driver = webdriver.Chrome(executable_path="f'{filepath}/chromedriver.exe'")
+    driver = webdriver.Chrome(executable_path="f{filepath}/chromedriver.exe")
     # driver = webdriver.Chrome(service=serv_obj)
     driver.implicitly_wait(10)
     return driver
