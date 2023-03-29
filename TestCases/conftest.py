5fbef1c5eee09b8480f19a0cf53918ce80100217
rock0232 <tony.5677774@gmail.com>
2023-03-29 10:42:37 +0530

    commit

--- TestCases/conftest.py
+++ TestCases/conftest.py
@@ -20,11 +20,11 @@ def setup():
     
     # live server 
     
-#     serv_obj = Service("/var/lib/jenkins/workspace/demo/google-chrome-stable_current_amd64.deb")
+    serv_obj = Service("/usr/local/bin/chromedriver.deb")
     
     # driver = webdriver.Chrome(executable_path="f{filepath}/chromedriver.exe")
 #     driver = webdriver.Chrome()
-    driver = webdriver.Chrome(f'{filepath}/chromedriver.exe')
+    driver = webdriver.Chrome(service=serv_obj)
     driver.implicitly_wait(10)
     return driver
 
