7a34857bbfb728cf2c1af6fa627340f551ebd410
rock0232 <tony.5677774@gmail.com>
2023-04-21 12:21:59 +0530

    change in conftest.py

--- TestCases/conftest.py
+++ TestCases/conftest.py
@@ -22,8 +22,8 @@ def setup():
     ###
     chrome_options.add_argument('--no-sandbox')
     chrome_options.add_argument('--disable-dev-shm-usage')
-    # driver = webdriver.Chrome(ChromeDriverManager().install(),options=chrome_options)
-    driver = webdriver.Chrome(options=chrome_options)
+    driver = webdriver.Chrome(ChromeDriverManager().install(),options=chrome_options)
+    # driver = webdriver.Chrome(options=chrome_options)
     driver.implicitly_wait(10)
     yield driver
     # driver.quit()
