1174afdf96d6971e7d8df5b6a09b8940a5ed13cf
rock0232 <tony.5677774@gmail.com>
2023-04-21 12:12:57 +0530

    change in conftest.py

--- TestCases/conftest.py
+++ TestCases/conftest.py
@@ -16,6 +16,10 @@ def setup():
     global driver
     chrome_options = Options()
     chrome_options.add_argument('--headless')
+    ###
+    chrome_options.add_argument("--disable-extensions")
+    chrome_options.add_argument("--disable-gpu")
+    ###
     chrome_options.add_argument('--no-sandbox')
     chrome_options.add_argument('--disable-dev-shm-usage')
     # driver = webdriver.Chrome(ChromeDriverManager().install(),options=chrome_options)
